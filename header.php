9bcbc9c81190d7589d258ab55e4eaaf3a6b4633a
MyDy-05-Paranitharan <84116658+mydynamica05@users.noreply.github.com>
2024-12-09 14:00:39 +0530

    Update header.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <a href="home.php">Home</a>
 <a href="about.php">About</a>
 <a href="portfolio.php">portfolio</a>
-<a href="pricing.php">pricing</a>
+<a href="pricing.php">pricing</a>fwfwef
 <a href="contact.php">contact</a>
 </nav>
 
